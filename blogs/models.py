6975e8f109aa580984901d27d4b41597f2d7005f
渡辺仁志 <al15110@shibaura-it.ac.jp>
2018-08-23 02:50:34 +0700

    createsuper user

--- blogs/models.py
+++ blogs/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    auther = models.ForeignKey(
+    author = models.ForeignKey(
         'auth.User',
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
